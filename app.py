6b28165329481c320313fec6636cc0785a079103
Miguel Cam <miguel.cam.mx@gmail.com>
2021-01-01 14:15:12 -0600

    Desing webapp. :construction_worker:

--- app.py
+++ app.py
@@ -1,19 +1,26 @@
 import requests
 import configparser
-from flask import Flask
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 @app.route('/')
 def weather_dashboard():
-  return "Hi!"
+  return render_template('home.html')
 
-@app.route('/results')
+@app.route('/results',  methods=['POST'])
 def render_results():
-    return "Results page"
+    zip_code = request.form['zipCode']
 
-if __name__ == '__main__':
-    app.run()
+    api_key = get_api_key()
+    data =  get_weather_results(zip_code, api_key)
+    temp = "{0:.2f}".format(data["main"]["temp"])
+    feels_like = "{0:.2f}".format(data["main"]["feels_like"])
+    weather = data["weather"][0]["main"]
+    location = data["name"]
+
+    return render_template('results.html', location=location, 
+                            temp=temp, feels_like=feels_like, weather=weather)
 
 
 def get_api_key():
@@ -22,8 +29,11 @@ def get_api_key():
     return config['openweathermap']['api']
 
 def get_weather_results(zip_code, api_key):
-    api_url = "http://api.openweathermap.org/data/2.5/weather?zip={}&appid={}".format(zip_code, api_key)
+    api_url = "http://api.openweathermap.org/data/2.5/weather?zip={}&units=imperial&appid={}".format(zip_code, api_key)
     r = requests.get(api_url)
     return r.json()
 
-print(get_weather_results("95129", get_api_key()))
+if __name__ == '__main__':
+    app.run()
+
+
